430f9a87211689e10dccd20d84c4eee34ec9def6
Rafy <Rafy@DESKTOP-ADC4ST3>
2019-03-28 17:53:52 +0200

    Solving the problem and get accepted

--- Sparse Arrays Hacker rank/Program.cs
+++ Sparse Arrays Hacker rank/Program.cs
@@ -8,8 +8,56 @@ namespace Sparse_Arrays_Hacker_rank
 {
     class Program
     {
+        // Complete the matchingStrings function below.
+        static int[] matchingStrings(string[] strings, string[] queries)
+        {
+            int[] result = new int[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < strings.Length; j++)
+                {
+                   if( queries[i]== strings[j])
+                    {
+                        count++;
+                    }
+                }
+                result[i] = count;
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
+           // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int stringsCount = Convert.ToInt32(Console.ReadLine());
+
+            string[] strings = new string[stringsCount];
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                string stringsItem = Console.ReadLine();
+                strings[i] = stringsItem;
+            }
+
+            int queriesCount = Convert.ToInt32(Console.ReadLine());
+
+            string[] queries = new string[queriesCount];
+
+            for (int i = 0; i < queriesCount; i++)
+            {
+                string queriesItem = Console.ReadLine();
+                queries[i] = queriesItem;
+            }
+
+            int[] res = matchingStrings(strings, queries);
+
+            Console.WriteLine(string.Join("\n", res));
+
+          //  textWriter.Flush();
+           // textWriter.Close();
         }
     }
 }
